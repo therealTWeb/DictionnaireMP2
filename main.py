a1ec8b8c2f160208987447dddfc12dac7811d63d
therealTWeb <74051127+therealTWeb@users.noreply.github.com>
2020-11-09 07:31:21 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,6 @@
+submarines ={
+    "sm1"={"lifes"= 2, "position"= (x, y), "direction"= north}, 
+    "sm2" ={"lifes"= 2, "postition"= (x, y), "direction"= left}, 
+    "sm3" ={"lifes"= 2, "position"= (x, y), "direction"= south}
+    }
+
---
+++ main.ts
@@ -0,0 +1,1 @@
+let submarines =  {TODO: Set} 
